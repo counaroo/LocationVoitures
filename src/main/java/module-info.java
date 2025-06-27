92811fb9274c7b9d82fdd95b450209958565f15e
Couro WANE <adjacourowane@gmail.com>
2025-06-27 00:03:56 +0000

    hello

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.couro.sadio.locationvoitures.locationvoitures {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.couro.sadio.locationvoitures.locationvoitures to javafx.fxml;
+    exports com.couro.sadio.locationvoitures.locationvoitures;
+}
